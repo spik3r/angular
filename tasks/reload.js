68fd71bb5afbf0bdbc89bc3eced40e48c16b5a99
Roman Yakobnyuk <roman.yakobnyuk@gmail.com>
2015-10-02 16:16:23 +1000

    Clean up reload task

--- tasks/reload.js
+++ tasks/reload.js
@@ -4,14 +4,6 @@
  * Run grunt build tasks
  */
 var gulp                 = require('gulp');
-var path                 = require('path');
-var sq                   = require('streamqueue');
-var runSequence          = require('run-sequence');
 var livereload		 = require('gulp-livereload');
 
-require("gulp-grunt")(gulp);
-
-module.exports = function () {
-	console.log('Refreshing page');
-	livereload.reload();
-}
+module.exports = function () { livereload.reload(); }
