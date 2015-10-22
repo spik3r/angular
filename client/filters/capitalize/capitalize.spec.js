48a3a0d2032b060096719ec24018edff9ad075b8
Roman Yakobnyuk <roman.yakobnyuk@gmail.com>
2015-10-22 14:47:42 +1000

    Cover capitalize with unit tests

--- client/filters/capitalize/capitalize.spec.js
+++ client/filters/capitalize/capitalize.spec.js
@@ -10,9 +10,20 @@ describe('capitalize filter', function () {
     capitalize = $filter('capitalize');
   }));
 
-  it('should ...', function () {
-    var text = 'bangular is awesome';
+  it('should exist', function () {
     expect(1).toBe(1);
+  });
+
+  it('should capitalize first letter of every word in the string without parameter', function () {
+    expect(capitalize('test string')).toBe('Test String');
+  });
+
+  it('should capitalize first letter of the string with the parameter "first"', function () {
+    expect(capitalize('test string', 'first')).toBe('Test string');
+  });
+
+  it('should capitalize first letter of every word in the string with parameter "all"', function () {
+    expect(capitalize('test string', 'all')).toBe('Test String');
   });
 
 });
--- client/filters/search-filter/search-filter.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('eiFrontend')
-  .filter('searchFilter', function () {
-    return function (input) {
-      return input;
-    };
-  });
