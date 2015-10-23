3233c9e747e4b7f1081b6754928017038cf810e7
Roman Yakobnyuk <roman.yakobnyuk@gmail.com>
2015-10-23 13:17:56 +1000

    Add tableau service and produce sample graph on the dashboard page

--- client/views/dashboard/dashboard.controller.js
+++ client/views/dashboard/dashboard.controller.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('eiFrontend')
-  .controller('DashboardCtrl', function (Sidenav) {
+  .controller('DashboardCtrl', function (Sidenav, $window, $timeout, Tableau) {
     // Get access to sidenav object and show it
     Sidenav.show();
     this.sidenav = Sidenav;
+
+    Tableau.init('sampleGraph');
   });
