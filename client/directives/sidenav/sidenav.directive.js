ccd5308e71ce45307aa394b4494e8234d0886f3e
Roman Yakobnyuk <roman.yakobnyuk@gmail.com>
2015-11-23 17:32:22 +1000

    [GLAB-86] Menu is clickable and redirects to some pages

--- client/directives/sidenav/sidenav.directive.js
+++ client/directives/sidenav/sidenav.directive.js
@@ -1,69 +1,61 @@
 'use strict';
 
 angular.module('eiFrontend')
-  .directive('sidenav', function ($route, $rootScope, $location, $timeout, Log, Sidenav, Auth) {
-    return {
-      restrict: 'EA',
-      scope: {
-        menu: '='
-      },
-      templateUrl: 'directives/sidenav/sidenav.html',
-      link: function (scope) {
+    .directive('sidenav', function ($route, $rootScope, $location, $timeout, Log, Sidenav, Auth) {
+        return {
+            restrict: 'EA',
+            scope: {
+                menu: '='
+            },
+            templateUrl: 'directives/sidenav/sidenav.html',
+            link: function (scope, element) {
+
+                var currentRoute;
+                (function activateCurrentRoute() {
+                    // Get current route and activate relevant link;
+                    var currentRoute = $route.current.$$route.originalPath.split("/")[1];
+
+                    element.find("#menu-" + currentRoute).addClass("active");
+
+                    scope.navigateTo = function (element) {
+                        console.log(element);
+                    };
+                })();
 
 
+                var target;
+                var path;
+                element.find('.menu-item').on('click', function (event) {
 
-        //// Filter menu based on user permissions
-        //scope.filteredMenu = [];
-        //for (var i = 0; i < scope.menu.length; i++) {
-        //  if (Auth.match(scope.menu[i].role)) {
-        //    // If menu item has dropdown, filter it
-        //    if (scope.menu[i].dropdown && scope.menu[i].dropdown.length > 0) {
-        //      var tempDropdown = [];
-        //
-        //      for (var j = 0; j < scope.menu[i].dropdown.length; j++) {
-        //        if (Auth.match(scope.menu[i].dropdown[j].role)) {
-        //          tempDropdown.push(scope.menu[i].dropdown[j]);
-        //        }
-        //      }
-        //
-        //      if (tempDropdown.length > 0) {
-        //        scope.menu[i].dropdown = tempDropdown;
-        //      } else {
-        //        delete scope.menu[i].dropdown;
-        //      }
-        //
-        //      scope.filteredMenu.push(scope.menu[i]);
-        //    } else {
-        //      scope.filteredMenu.push(scope.menu[i]);
-        //    }
-        //  }
-        //}
-        //scope.menu = scope.filteredMenu;
-        //
-        //var sidenav = Sidenav;
-        //scope.currentRoute = $route.current.$$route.originalPath;
-        //scope.user = Auth.user;
-        //
-        //// When route is changed, update current route
-        //scope.$on('$routeChangeSuccess', function (event, route) {
-        //  scope.currentRoute = route.$$route.originalPath;
-        //});
-        //
-        //// When tab is clicked, redirect to that tab
-        //scope.$on('activateTab', function (event, tab) {
-        //  // Create redirect path from the tab name
-        //  Log.say('sidenav', 'Redirect to: ' + tab.path);
-        //
-        //  $timeout(function () {
-        //    $location.path(tab.path);
-        //    // Broadcast that tab has changed, so children can update their styles
-        //    scope.$broadcast('activateTabSuccess', tab);
-        //  }, 100);
-        //});
-        //
-        //scope.hide = function () {
-        //  sidenav.toggle();
-        //}
-      }
-    };
-  });
+                    target = event.currentTarget.id;
+                    // Remove active status from every tab
+                    element.find(".menu-item").removeClass('active');
+
+                    // Add active status to clicked tab
+                    element.find("#" + target).addClass('active');
+
+                    path = '/' + target.split('-')[1];
+
+                    if (path == "/dashboard" || path == "/logout" || path == "/settings") {
+                        $timeout(function () {
+
+                            Log.say('sidenav', 'Redirect to: ' + path);
+                            $location.path(path);
+                        }, 50);
+                    }
+                });
+
+                //// When route is changed, update current route
+                //scope.$on('$routeChangeSuccess', function (event, route) {
+                //    scope.currentRoute = route.$$route.originalPath;
+                //});
+                //
+                //// When tab is clicked, redirect to that tab
+                //scope.$on('activateTab', function (event, tab) {
+                //    // Create redirect path from the tab name
+                //
+                //});
+
+            }
+        };
+    });
