95ebdb712cd02b7ddeff472465b6b65cf4b06d8c
Roman Yakobnyuk <roman.yakobnyuk@gmail.com>
2015-12-01 14:35:17 +1000

    [GLAB-86] Cleanup sidenav styles

--- client/directives/followers-age/followers-age.directive.js
+++ client/directives/followers-age/followers-age.directive.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('eiFrontend')
-  .directive('followersAge', function () {
-    return {
-      restrict: 'EA',
-      templateUrl: 'directives/followers-age/followers-age.html',
-      link: function (scope, element) {
+    .directive('followersAge', function () {
+        return {
+            restrict: 'EA',
+            replace: true,
+            templateUrl: 'directives/followers-age/followers-age.html',
+            link: function (scope, element) {
 
-      }
-    };
-  });
+            }
+        };
+    });
--- client/directives/followers-gender/followers-gender.directive.js
+++ client/directives/followers-gender/followers-gender.directive.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('eiFrontend')
-  .directive('followersGender', function () {
-    return {
-      restrict: 'EA',
-      templateUrl: 'directives/followers-gender/followers-gender.html',
-      link: function (scope, element) {
+    .directive('followersGender', function () {
+        return {
+            restrict: 'EA',
+            replace: true,
+            templateUrl: 'directives/followers-gender/followers-gender.html',
+            link: function (scope, element) {
 
-      }
-    };
-  });
+            }
+        };
+    });
