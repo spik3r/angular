9756e14adbbf4ecfc55108023341c81f58ab0ffa
Roman Yakobnyuk <roman.yakobnyuk@gmail.com>
2015-12-08 15:10:15 +1000

    [GLAB-86] Nicely style new age and gender charts

--- client/directives/piechart/piechart.directive.js
+++ client/directives/piechart/piechart.directive.js
@@ -1,26 +1,21 @@
 'use strict';
 
+/**
+ * This directive describes a pie chart that takes percent value and two labels.
+ */
 angular.module('eiFrontend')
     .directive('piechart', function () {
         return {
             restrict: 'EA',
             replace: true,
             scope: {
-              percent: '='
+              percent: '=', // Percent value for first label
+              labels: '=' // Two labels to display (if more then two labels, they are ignored and first two are used)
             },
             templateUrl: 'directives/piechart/piechart.html',
             link: function (scope, element) {
-
-                var percent = parseInt(scope.percent),
-                    deg = 360*percent/100;
-
-                if (percent > 50) {
-                    element.addClass('over-50');
-                }
-
-                console.log(percent);
-
-                $('.pie-chart-fill').css('transform','rotate('+ deg +'deg)');
+                if (scope.percent > 50) { element.addClass('over-50'); }
+                element.find('.pie-chart-fill').css('transform','rotate('+ 360*scope.percent/100 +'deg)');
             }
         };
     });
--- client/directives/slicechart/slicechart.directive.js
+++ client/directives/slicechart/slicechart.directive.js
@@ -5,30 +5,33 @@ angular.module('eiFrontend')
         return {
             restrict: 'EA',
             replace: true,
+            scope: {
+              slices: '='
+            },
             templateUrl: 'directives/slicechart/slicechart.html',
             link: function (scope, element) {
-
-                var age_groups = [  ['< 18',    20],
-                                    ['18-24',   20],
-                                    ['25-34',   30],
-                                    ['35-44',   10],
-                                    ['45-55',   10],
-                                    ['60+',     10] ];
-
-                // Create relative ages object to display:
-                scope.ages = {};
-
+                // Create relative slice sizes to display:
                 // 1. Find biggest age group
-                var biggest_age_group = age_groups[0];
-                for (var i = 1; i < age_groups.length; i++) {
-                    if (age_groups[i][1] > biggest_age_group[1]) {
-                        biggest_age_group = age_groups[i];
+                var biggest_slice_size = null;
+                var slice;
+                for (slice in scope.slices) {
+                    // This check it not required, but prevents IDEA from highlighting errors
+                    if (scope.slices.hasOwnProperty(slice)) {
+                        if (!biggest_slice_size) {
+                            biggest_slice_size = scope.slices[slice];
+                        } else {
+                            if (scope.slices[slice] > biggest_slice_size) {
+                                biggest_slice_size = scope.slices[slice];
+                            }
+                        }
                     }
                 }
-
                 // 2. Scale other groups according to biggest group
-                for (var j = 0; j < age_groups.length; j++) {
-                    scope.ages[age_groups[j][0]] = 100 / biggest_age_group[1] * age_groups[j][1];
+                scope.ages = {};
+                for (slice in scope.slices) {
+                    if (scope.slices.hasOwnProperty(slice)) {
+                        scope.ages[slice] = 100 / biggest_slice_size * scope.slices[slice];
+                    }
                 }
             }
         };
