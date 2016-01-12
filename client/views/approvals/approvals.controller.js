f5a19f61b045a2564018c3b9a3e3a75ab5418674
Roman Yakobnyuk <roman.yakobnyuk@gmail.com>
2016-01-12 13:29:02 +1000

    Remove unneeded routes to clear project

--- client/views/approvals/approvals.controller.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('ei.console')
-  .controller('ApprovalsCtrl', function (Sidenav) {
-    // Get access to sidenav object and show it
-    Sidenav.show();
-    this.sidenav = Sidenav;
-  });
--- client/views/track/track.controller.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('ei.console')
-  .controller('TrackCtrl', function (Sidenav) {
-    // Get access to sidenav object and show it
-    Sidenav.show();
-    this.sidenav = Sidenav;
-  });
